d190ec6dd206e690ce30005ff37e7ea667b48fd5
raviteja-chandu <30772421+raviteja-chandu@users.noreply.github.com>
2017-08-21 22:22:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article_1', function (req, res) {
-  res.send("article one here, created by RC");
+  res.sendFile(path.join(__dirname, 'ui', 'article1.html'));
 });
 app.get('/article_2', function (req, res) {
   res.send("article two here, created by RC");
