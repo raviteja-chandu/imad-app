3f5e069a2ae537d7ea872f6766d87f50a89be45b
raviteja-chandu <30772421+raviteja-chandu@users.noreply.github.com>
2017-08-23 20:08:18 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+  res.sendFile(path.join(__dirname, 'ui', 'article2.html'));
 });
 //app.get('/:articleName', function (req, res) {
   //  var articleName= req.params.articleName;
